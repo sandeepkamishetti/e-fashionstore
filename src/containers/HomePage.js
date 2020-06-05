77258c00799bb593116c970efacee68491b9f8ce
sandeepkamishetti <59001664+sandeepkamishetti@users.noreply.github.com>
2020-06-05 19:13:37 +0530

    Update HomePage.js

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -6,7 +6,8 @@ import Shop from '../components/Home/Shop'
 const HomePage = (props) => {
     return(
         <React.Fragment> {/* React.Fragment does not create a DOM element at all */}
-            <Header/>
+        <h1>This site is deployed by Sandeep Kumar KAMISHETTI</h1>    
+        <Header/>
             <Shop/>
             <Footer/>
         </React.Fragment>
